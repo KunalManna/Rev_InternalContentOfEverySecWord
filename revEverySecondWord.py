df746386c0cb16bb7ec830a764808cae9239fe60
Kunal Manna <117971514+KunalManna@users.noreply.github.com>
2022-11-24 21:26:37 +0530

    Add files via upload

---
+++ revEverySecondWord.py
@@ -0,0 +1,22 @@
+# Revese every second word a string
+
+def revEverySecWord(s):
+    p=s.split()
+    n=len(p)
+    i=0
+    l=[]
+    while(i<n):
+        if i%2==0:
+            l.append(p[i])
+        else:
+            l.append(p[i][::-1])
+        i=i+1
+    output=' '.join(l)
+    return output
+    
+
+
+
+s=input("Enter the string:\n")               # one two three four five six
+ans=revEverySecWord(s)
+print(ans)                                   # one owt three ruof five xis
